1460ce95cf2c35a2553bf42635181139c189e8a5
techicode <techicode@outlook.com>
2022-09-18 12:40:13 -0300

    entry point for all the routers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import express from "express";
+import express from 'express';
+import { directorRouter } from './routes/director.routes.js';
+import { movieRouter } from './routes/movie.routes.js';
+import { producerRouter } from './routes/producer.routes.js';
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/movies', movieRouter);
+app.use('/directors', directorRouter);
+app.use('/producers', producerRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
