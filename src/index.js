316ed27d6b88ef018b17622c790d7e82dfaf844f
techicode <techicode@outlook.com>
2022-09-18 21:34:32 -0300

    added an option to limit request per x minutes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
+
 import { NOT_ALLOWED_METHODS } from './constants/allowed-methods.js';
+import { LIMITER_OPTIONS } from './constants/limiter.js';
 import { directorRouter } from './routes/director.routes.js';
 import { movieRouter } from './routes/movie.routes.js';
 import { producerRouter } from './routes/producer.routes.js';
@@ -10,6 +12,9 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Apply the rate limiting middleware to all requests
+app.use(LIMITER_OPTIONS);
+
 app.use('*', (req, res, next) => {
   if (NOT_ALLOWED_METHODS.has(req.method)) {
     return res.status(405).send('Method Not Allowed');
@@ -17,6 +22,8 @@ app.use('*', (req, res, next) => {
 
   return next();
 });
+
+// TODO limit requests per minute or something
 
 app.use('/movies', movieRouter);
 app.use('/directors', directorRouter);
