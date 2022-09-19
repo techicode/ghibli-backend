a00036847834884624254d63d4632003632168e9
techicode <techicode@outlook.com>
2022-09-18 21:24:18 -0300

    blocked unallowed methods

---
+++ src/constants/allowed-methods.js
@@ -0,0 +1,9 @@
+export const NOT_ALLOWED_METHODS = new Set([
+  'PUT',
+  'HEAD',
+  'POST',
+  'DELETE',
+  'PATCH',
+  'OPTIONS',
+  'PROPFIND',
+]);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { NOT_ALLOWED_METHODS } from './constants/allowed-methods.js';
 import { directorRouter } from './routes/director.routes.js';
 import { movieRouter } from './routes/movie.routes.js';
 import { producerRouter } from './routes/producer.routes.js';
@@ -8,6 +9,14 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('*', (req, res, next) => {
+  if (NOT_ALLOWED_METHODS.has(req.method)) {
+    return res.status(405).send('Method Not Allowed');
+  }
+
+  return next();
+});
 
 app.use('/movies', movieRouter);
 app.use('/directors', directorRouter);
