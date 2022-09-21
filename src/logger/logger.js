d208e83863e7179890b5400421c2bdd80e448d86
techicode <techicode@outlook.com>
2022-09-21 01:29:14 -0300

    added logger

---
+++ src/logger/logger.js
@@ -0,0 +1,21 @@
+import { createLogger, format, transports } from 'winston';
+
+export const logger = createLogger({
+  level: 'error',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
+    // - Write all logs error (and below) to `quick-start-error.log`.
+    //
+    new transports.File({ filename: './log/error.log'}),
+  ],
+});
+
